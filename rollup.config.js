68c9188ab631854b58ca6b8474d9b97539a32fb8
18063323221 <1475929340@qq.com>
2025-06-19 16:56:44 +0800

    .

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,12 @@ import serve from "rollup-plugin-serve";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+//nodejs在esm模式下获取当前文件路径
+//‌在ES模块（ESM）中获取当前文件的绝对路径可以通过import.meta.url和fileURLToPath函数实现。
 const __filename = fileURLToPath(import.meta.url); //当前文件的绝对路径
+//首先，import.meta.url返回当前模块的URL，格式为file:///协议开头。
+// 例如，如果当前模块的路径是file:///C:/path/to/file.js，则import.meta.url会返回这个完整的URL。
+// 接下来，可以使用Node.js的url模块中的fileURLToPath函数将这个URL转换为文件系统路径。
 const __dirname = dirname(__filename);
 
 export default {
