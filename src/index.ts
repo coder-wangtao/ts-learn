f6b590a6b37cc80124cc8dd868eead919bdd79e9
18063323221 <1475929340@qq.com>
2025-06-20 16:04:16 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
 const a: string = "111";
 console.log(a);
+
+let name = "1111";
+let s1: String = new String("abc");
+let s2: String = "abc";
+let age: number;
+
+const enum USER_ROLE {
+  USER,
+  ADMIN,
+}
+
+console.log(USER_ROLE.USER);
+
+function validate(val: never) {}
+
+function handle(a: string | number | boolean) {
+  if (typeof a === "string") {
+    return a;
+  }
+  if (typeof a === "number") {
+    return a;
+  }
+  if (typeof a === "boolean") {
+    return a;
+  }
+  validate(a);
+}
+console.log("aad");
+
+export {};
+
+let union: string | number | boolean | never;
