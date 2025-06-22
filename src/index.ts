940a3ac23c55fe03f1f0e1210439161bedd2ca70
18063323221 <1475929340@qq.com>
2025-06-22 22:20:20 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,34 @@ console.log("aad");
 export {};
 
 let union: string | number | boolean | never;
+
+let person = {
+  name: "wangtao",
+  age: 20,
+};
+//采用ts中的typeof来获取变量的类型,
+//ts中的this类型需要手动指定，默认是函数的第一个参数
+type IThis = typeof person;
+
+function getVal(this: IThis, key: keyof IThis) {
+  return this[key];
+}
+
+let n = getVal.call(person, "name");
+let b = getVal.call(person, "age");
+
+//ts中的函数重载是伪重载，是类型的重载，而不是逻辑的重载
+function toArray(value: string): string[];
+function toArray(value: number): number[];
+function toArray(value: number | string): string[] | number[] {
+  if (typeof value === "string") {
+    return value.split("");
+  } else {
+    return value
+      .toString()
+      .split("")
+      .map((item) => Number(item));
+  }
+}
+let arr = toArray(123);
+let arr1 = toArray("123");
